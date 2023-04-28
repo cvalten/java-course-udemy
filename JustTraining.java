2874765da79732d9c29570c393391b6b9069216c
Cees van Alten <info@ceesvanalten.nl>
2023-04-28 14:12:20 +0200

    Justtraining.

---
+++ JustTraining.java
@@ -0,0 +1,15 @@
+public class JustTraining {
+
+    public static double blaat = 2.718281828459045;
+    public static int testNumber(){
+        return 1;
+
+    }
+    public static void main(String[] args){
+     System.out.println("hello123".toUpperCase());
+     System.out.println(testNumber());
+     System.out.println(blaat);
+     blaat = 4.0;
+        System.out.println(blaat);
+    }
+}
